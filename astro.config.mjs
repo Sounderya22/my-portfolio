64e830ccce3d0f509b92374b15a49c54608274a3
Sounderya22 <vvsounderya22@gmail.com>
2026-01-07 01:30:22 +0530

    Fixed CSS issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
-// @ts-check
 import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   site: 'https://sounderya22.github.io',
-  base: '/my-portfolio', // Replace with your repo name
+  base: '/my-portfolio',
+  vite: {
+    plugins: [tailwindcss()], // This is required for Tailwind v4
+  },
 });
