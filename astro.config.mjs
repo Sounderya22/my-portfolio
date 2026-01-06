0da6e53aabf3ccbff9d487afc09ab7986385e0fe
Sounderya22 <vvsounderya22@gmail.com>
2026-01-07 01:23:47 +0530

    Fix base path for CSS

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,6 @@ import { defineConfig } from "astro/config";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  site: 'https://Sounderya22.github.io',
+  site: 'https://sounderya22.github.io',
   base: '/my-portfolio', // Replace with your repo name
 });
